c64ffe73bf93a67277a0638665c79f8840d5ce3f
Shamnaros <shamnaros1412@gmail.com>
2023-03-08 18:42:32 +0530

    commit-28

--- src/app/orders/table/table.component.ts
+++ src/app/orders/table/table.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import * as VANTA from 'vanta';
+import * as VANTA from 'vanta/dist/vanta.birds.min.js';
+// import * as THREE from 'three';
+// import * as VANTA from 'vanta';
+// import VANTA from 'vanta/dist/vanta.birds.min.js'
+
+
+
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
+export class TableComponent implements OnInit, OnDestroy {
 
+  private vantaInstance: any;
+
+  constructor() {
+   
+    // VANTA.BIRDS('#element');
+  }
+  ngOnInit() {
+    this.vantaInstance = VANTA.BIRDS({
+      el: '#vanta-element',
+      backgroundColor: 0x141414,
+      color1: 0xff0000,
+      color2: 0xffffff,
+      colorMode: 'lerpGradient',
+      birdSize: 1.20,
+      wingSpan: 20.00,
+      speedLimit: 6.00,
+      separation: 70.00,
+      alignment: 80.00,
+      cohesion: 75.00
+    });
+    this.vantaInstance = VANTA.BIRDS('#element3')
+  }
+
+  ngOnDestroy() {
+    if (this.vantaInstance) {
+      this.vantaInstance.destroy();
+    }
+  }
 }
